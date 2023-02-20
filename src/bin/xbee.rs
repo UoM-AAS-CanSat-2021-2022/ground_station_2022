0a58d0a3e3809a027dcca3ac25bf0fb0ab21e9e0
Sam <tritoke@protonmail.com>
2023-02-20 13:42:47 +0000

    ✨ modified xbee binary to send a telemetry file over the XBee.

--- src/bin/xbee.rs
+++ src/bin/xbee.rs
@@ -1,32 +1,38 @@
 use anyhow::Result;
+use ground_station::telemetry::Telemetry;
 use ground_station::xbee::{TxRequest, XbeePacket};
-use hex_literal::hex;
-use serialport::SerialPortType;
 use std::io::Write;
+use std::thread;
 use std::time::Duration;
 
 fn main() -> Result<()> {
-    // let ports = serialport::available_ports().unwrap();
-    // let port = ports
-    //     .into_iter()
-    //     .filter(|port| matches!(port.port_type, SerialPortType::UsbPort(_)))
-    //     .next()
-    //     .unwrap();
+    let usb_port = std::env::args()
+        .nth(1)
+        .expect("Need at least 2 arguments - first is port, second is the telemetry file");
+    let mut sport = serialport::new(dbg!(usb_port), 230400).open()?;
 
-    let req = TxRequest::new(0x00_00, "CMD,1047,CAL");
-    let mut packet: XbeePacket = req.try_into().unwrap();
-    packet.set_frame_id(1);
-    eprintln!("{packet:02X?}");
-    let ser = packet.serialise()?;
-    eprintln!("{ser:02X?}");
+    let fname = std::env::args()
+        .nth(2)
+        .expect("Need one more argument - the telemetry file");
+    let file_data = std::fs::read_to_string(fname)?;
+    let telemetry: Vec<Telemetry> = file_data
+        .lines()
+        .map(str::parse)
+        .collect::<Result<_, _>>()?;
 
-    let mut sport = serialport::new("/dev/ttyUSB0", 230400).open()?;
-    sport.write(&ser)?;
-    let mut buf = [0u8; 1024];
-    sport.set_timeout(Duration::from_secs(2))?;
-    let bytes_read = sport.read(&mut buf)?;
-    eprintln!("{:02X?}", &buf[..bytes_read]);
-    dbg!(XbeePacket::decode(&buf[..bytes_read]));
+    let mut frame_id = 0;
+    for telem in telemetry {
+        let req = TxRequest::new(0xFFFF, format!("{telem}"));
+        let mut packet: XbeePacket = req.try_into().unwrap();
+        packet.set_frame_id(frame_id);
+        frame_id.wrapping_add(1);
+        eprintln!("created packet: {packet:02X?}");
+        let ser = packet.serialise()?;
+        eprintln!("sending: {ser:02X?}");
+        sport.write(&ser)?;
+
+        thread::sleep(Duration::from_secs(1));
+    }
 
     Ok(())
 }
